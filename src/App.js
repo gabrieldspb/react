cd813abbdee5b076fc83e97aac98231b3bb42fcb
Gabriel <gabrieldspb@gmail.com>
2023-10-18 21:11:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  const meuNome = 'Gabriel';
+
+  return (
+    <div className="container">
+      <h1 className="nome">Ola, {meuNome}!</h1>
+    </div>
+  );
+}
+
+export default App;
